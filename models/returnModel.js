0d0d46d8be8743c92dbeb2ff08d614194b947f4b
Muhammed Ashif <ashif.achuttan@gmail.com>
2024-08-08 10:55:22 +0530

    Return Order Initial stage

---
+++ models/returnModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const returnItemSchema = new mongoose.Schema(
+    {
+        userId : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'User'
+        },
+        orderId : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Order'
+        },
+        productId : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Product'
+        },
+        reason : {
+            type : String
+        },
+        status : {
+            type : String,
+            default : 'Return Requested'
+        }
+    }
+)
+
+const ReturnItem = mongoose.model('returnItem',returnItemSchema);
+
+module.exports = ReturnItem;
