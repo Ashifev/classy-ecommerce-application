17931b23f9338d2c4947ebb9b1900e4826f652f7
Muhammed Ashif <ashif.achuttan@gmail.com>
2024-10-08 19:12:40 +0530

    complete

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true})
 .then(()=>{
     console.log("Database Connected");
 })
